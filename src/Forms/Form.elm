90bfd1c9f20dcd1a7ab696db94a8dc4664448bba
ozmat <m.labatino@gmail.com>
2018-02-05 07:00:32 +0100

    Add docs for Form

--- src/Forms/Form.elm
+++ src/Forms/Form.elm
@@ -6,28 +6,50 @@ module Forms.Form
         , validateD
         )
 
+{-| `Form` is the top level type of the library. It is built with a [`Group`](http://package.elm-lang.org/packages/ozmat/elm-forms/latest/Forms-Field#Group)
+of `Field`s and a [`Validate`](http://package.elm-lang.org/packages/ozmat/elm-forms/latest/Forms-Validation#Validate) function.
+Please refer to the [basic examples]() (or [advanced examples]()) for a better
+understanding
+
+
+# Definition
+
+@docs Form
+
+
+# Common Helpers
+
+@docs form, validate, validateD
+
+-}
+
 import Dict as D exposing (Dict)
 import Validation as VA
 import Forms.Field as F exposing (Group)
 import Forms.Validation as V exposing (Validate, FieldError)
 
 
--- Form
-
-
+{-| A `Form` is a `Group` of `Field`s and a `Validate` function
+-}
 type Form comparable err a
     = Form (Group comparable) (Validate comparable err a)
 
 
+
+-- Common Helpers
+
+
+{-| Helps creating a `Form`
+-}
 form : Group comparable -> Validate comparable err a -> Form comparable err a
 form =
     Form
 
 
-
--- Validation
-
-
+{-| Helps validating a `Form`. This will use the internal `Validate` function
+and `Field`s to run the validation process, then it will convert the
+`FormValidation` into a [`Result`](http://package.elm-lang.org/packages/elm-lang/core/latest/Result#Result).
+-}
 validate : Form comparable err a -> Result (List ( comparable, FieldError err )) a
 validate (Form fields validate) =
     validate fields
@@ -35,6 +57,9 @@ validate (Form fields validate) =
         |> Result.mapError (List.map V.toTuple)
 
 
+{-| Equivalent to the previous one but returns a
+`Dict` of error instead of a `List`.
+-}
 validateD : Form comparable err a -> Result (Dict comparable (FieldError err)) a
 validateD fo =
     validate fo
